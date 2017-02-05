4f37de28d85e1cbbe892fda2b83b402844e26a29
wowbagger84 <wowbagger84@gmail.com>
2017-02-05 11:13:05 +0100

    Added credits, auto blink function

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+    Animator anim;
+
+    [Header("Blink")]
+    [SerializeField]
+    private float minBlinkTime = 0.5f;
+    [SerializeField]
+    private float maxBlinkTime = 5f;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        Blink();
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+		//TODO: set correct animation state for what is happening
+	//}
+
+    /// <summary>
+    /// Simple self calling class to make the character blink
+    /// </summary>
+    void Blink()
+    {
+        anim.SetTrigger("Blink");
+        float rndTime = Random.Range(minBlinkTime, maxBlinkTime);
+        Invoke("Blink", rndTime);
+    }
+}
