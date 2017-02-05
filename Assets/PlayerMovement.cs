7574850bf68a8d3c6b9e202bcb038f58e8c6ac68
Robert Esbjörnsson <fotbollsrobert@hotmail.com>
2017-02-05 10:35:44 +0100

    init commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody2D rb2D;
+    SpriteRenderer spR;
+
+    public float jumpPower = 10;
+
+	// Use this for initialization
+	void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+        spR = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal") * 12;
+        //float y = Input.GetAxis("Vertical");
+
+        rb2D.velocity = new Vector3(x, rb2D.velocity.y, 0);
+        if (x > 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (x < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2D.AddForce(Vector3.up * jumpPower, ForceMode2D.Impulse);
+        }
+
+    }
+}
